a24f34ad2f8eb936722df0b781eb18c04de16210
danielev1976 <128916523+danielev1976@users.noreply.github.com>
2026-02-02 14:23:08 +0100

    Add heading for 'My Recipes' section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Recipes from "@/app/recipes/page";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h2>My Recipes</h2>
      <Recipes/>
     </div>
   );
