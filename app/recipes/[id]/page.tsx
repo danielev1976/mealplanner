064110cfaf2778ba25f80ebab5434e38e9a421da
danielev1976 <daniellevin1976@gmail.com>
2026-02-03 16:55:51 +0100

    modify again and again

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -7,7 +7,7 @@ export default async function RecipePage({
 
   return (
     <main className="max-w-7xl mx-auto px-6 py-10">
-      <h1>Recipe Details</h1>
+      <h1 className="text-white">Recipe Details</h1>
       <p>Displaying recipe for ID: <strong>{id}</strong></p>
       
       {/* This is where you would fetch your data using the ID */}
